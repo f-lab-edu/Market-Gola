6210c4902e94141dfba7064feddac65ad7e9811f
jiwoo <sgo8308@gmail.com>
2022-06-04 20:54:41 +0900

    초기 구축 - 프로젝트 폴더 구성

---
+++ src/main/java/com/flab/marketgola/MarketGolaApplication.java
@@ -0,0 +1,13 @@
+package com.flab.marketgola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketGolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketGolaApplication.class, args);
+	}
+
+}
