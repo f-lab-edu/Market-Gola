18e165b2c12fa520621c31233080b1139f31a290
jieun-dev1 <jkdev1324@gmail.com>
2022-06-08 12:50:08 +0900

    초기구축 - 프로젝트 폴더 구성

---
+++ src/main/java/com/marketGola/MarketgolaApplication.java
@@ -0,0 +1,11 @@
+package com.marketGola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketgolaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarketgolaApplication.class, args);
+    }
+}
