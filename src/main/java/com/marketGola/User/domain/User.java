f6877bdadcc1cc5dc4015e9371453a220efc831a
jieun-dev1 <jkdev1324@gmail.com>
2022-06-13 15:34:05 +0900

    feat: User 클래스와 생성자 추가

--- src/main/java/com/marketGola/User/domain/User.java
+++ src/main/java/com/marketGola/User/domain/User.java
@@ -1,4 +1,27 @@
 package com.marketGola.User.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 }
