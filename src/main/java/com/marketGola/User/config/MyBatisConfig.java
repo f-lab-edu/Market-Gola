20cc93801a26e5be4a95153afb22b1ff97d0e4f5
jieun-dev1 <jkdev1324@gmail.com>
2022-06-13 15:33:16 +0900

    chore: 불필요한 import 삭제

--- src/main/java/com/marketGola/User/config/MyBatisConfig.java
+++ src/main/java/com/marketGola/User/config/MyBatisConfig.java
@@ -5,31 +5,18 @@ import com.marketGola.User.repository.mybatis.MyBatisUserRepository;
 import com.marketGola.User.repository.mybatis.UserMapper;
 import com.marketGola.User.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 //UserMapper 를 주입받고, 필요한 의존관계 생성.
 @Configuration
 @RequiredArgsConstructor
-
 public class MyBatisConfig {
     private final UserMapper userMapper;
 
     @Bean
     public UserService userService() {
         return new UserService(userRepository());
-
     }
 
     @Bean
