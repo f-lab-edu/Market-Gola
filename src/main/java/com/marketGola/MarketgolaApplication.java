6a19aefd7eb086706e766d573688a20dc2c8a935
jieun-dev1 <jkdev1324@gmail.com>
2022-06-13 15:32:54 +0900

    chore: MyBatisConfig import 추가

--- src/main/java/com/marketGola/MarketgolaApplication.java
+++ src/main/java/com/marketGola/MarketgolaApplication.java
@@ -1,11 +1,25 @@
 package com.marketGola;
 
+import com.marketGola.User.config.MyBatisConfig;
+import com.marketGola.User.repository.UserRepository;
+import com.marketGola.User.service.TestDataInit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Import(MyBatisConfig.class)
+@SpringBootApplication(scanBasePackages = "com.marketGola.User.controller")
 public class MarketgolaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketgolaApplication.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public TestDataInit testDataInit(UserRepository userRepository) {
+        return new TestDataInit(userRepository);
     }
 }
