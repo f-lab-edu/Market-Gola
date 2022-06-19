960019cde8b5b92078339a9cb0d9c3af18631ea3
jieun-dev1 <jkdev1324@gmail.com>
2022-06-19 16:11:17 +0900

    refactor (user): @bean 제거 (#3)
    MarketGolaApplication
    -기존 구조는 controller 만 스캔하고 service, repository는 수동 빈 등록 하였음. 효율적이지 않다고 판단하여 전체 컴포넌트 스캔으로 대체함.
    
    MyBatisConfig
    -@bean 이 수동으로 중복등록 되어서 제거함.

--- src/main/java/com/marketGola/MarketgolaApplication.java
+++ src/main/java/com/marketGola/MarketgolaApplication.java
@@ -1,15 +1,14 @@
 package com.marketGola;
 
-import com.marketGola.User.config.MyBatisConfig;
+import com.marketGola.user.config.MyBatisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @Import(MyBatisConfig.class)
-@SpringBootApplication(scanBasePackages = "com.marketGola.User.controller")
+@SpringBootApplication
 public class MarketgolaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketgolaApplication.class, args);
     }
-
 }
