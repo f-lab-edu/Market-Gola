9ac399320b84487783585be79b9075d7a59911d9
jiwoo <sgo8308@gmail.com>
2022-06-20 18:17:22 +0900

    feat(user, common): 패키지 특화 ControllerAdvice 삭제 후 GlobalControllerAdvice 기능 추가 (#5)
    exceptionHandler()
    모든 예외를 BaseException을 상속한 예외로 감싸서 던질 시 모든 예외를 처리가능
    
    doLog()
    exception의 타입에 따라 다른 log level로 로깅
    
    createErrorResult()
    일관된 예외 결과를 만들어주며 예외가 갖고 있는 responseHttpStatus를 응답상태코드로 이용

--- src/main/java/com/flab/marketgola/common/exception/GlobalControllerAdvice.java
+++ src/main/java/com/flab/marketgola/common/exception/GlobalControllerAdvice.java
@@ -1,12 +1,53 @@
 package com.flab.marketgola.common.exception;
 
+import com.flab.marketgola.common.util.TimeUtil;
+import java.time.LocalDateTime;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 공통적인 API 예외 처리를 모아 놓는 클래스
  */
-@RestControllerAdvice
+@Slf4j
+@RestControllerAdvice()
 public class GlobalControllerAdvice {
 
+    /**
+     * 예외 처리 핸들러 BaseException 하위의 예외를 받아서 예외 결과를 일관되게 반환한다.
+     */
+    @ExceptionHandler()
+    public ResponseEntity<ErrorResult> exceptionHandler(BaseException e,
+            HttpServletRequest request) {
+        doLog(e, request);
+        return createErrorResult(e);
+    }
 
+    /**
+     * 예외 종류에 따라 다른 level로 로그를 찍는 메소드.
+     */
+    private void doLog(BaseException e, HttpServletRequest request) {
+        if (e instanceof FatalException) {
+            log.error("요청 uri = {}, http method = {}", request.getRequestURI(),
+                    request.getMethod(), e);
+        } else {
+            log.debug("요청 uri = {}, http method = {}", request.getRequestURI(),
+                    request.getMethod(), e);
+        }
+    }
+
+    /**
+     * 예외 결과를 만들어주는 메소드
+     */
+    private ResponseEntity<ErrorResult> createErrorResult(BaseException e) {
+        String timestamp = TimeUtil.convert(LocalDateTime.now());
+        int status = e.getResponseHttpStatus().value();
+        String error = e.getResponseHttpStatus().getReasonPhrase();
+        String type = e.getClass().getSimpleName();
+        String message = e.getMessage();
+        ErrorResult errorResult = new ErrorResult(timestamp, status, error, type, message);
+        return new ResponseEntity<>(errorResult, e.getResponseHttpStatus());
+    }
 }
