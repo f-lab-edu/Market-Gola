b02c13ab8409a5695169df01251f1501326a26d7
jiwoo <sgo8308@gmail.com>
2022-06-20 18:20:29 +0900

    feat(user): SampleErrorController mapping 추가, SampleErrorResult 삭제 (#5)
    SampleErrorController
    FatalException과 GeneralException을 테스트하기 위한 mapping 메소드 추가
    
    SampleErrorResult
    삭제 후 ErrorResult 추가

--- src/main/java/com/flab/marketgola/user/controller/SampleErrorController.java
+++ src/main/java/com/flab/marketgola/user/controller/SampleErrorController.java
@@ -1,13 +1,20 @@
 package com.flab.marketgola.user.controller;
 
+import com.flab.marketgola.user.exception.SampleFatalException;
+import com.flab.marketgola.user.exception.SampleGeneralException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleErrorController {
 
-    @GetMapping("/null-exception")
+    @GetMapping("/general")
     public String nullException() {
-        throw new NullPointerException("널 값입니다.");
+        throw new SampleGeneralException("일반적인 예외입니다.");
+    }
+
+    @GetMapping("/fatal")
+    public String fatalException() {
+        throw new SampleFatalException("심각한 예외입니다.");
     }
 }
