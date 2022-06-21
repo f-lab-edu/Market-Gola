e4aeb2f96cff42484d09e5eca54a374742372e7e
jiwoo <sgo8308@gmail.com>
2022-06-21 15:51:44 +0900

    test(user): SampleRepositoryTest가 application-unit을 이용하도록 지정 (#10)

--- src/test/java/com/flab/marketgola/user/repository/SampleRepositoryTest.java
+++ src/test/java/com/flab/marketgola/user/repository/SampleRepositoryTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+@ActiveProfiles("unit") // 어떤 property 파일을 이용해서 테스트할 것인지 명시(active된 profile에 상관 없이 동작)
 @Transactional
 @SpringBootTest
 @RequiredArgsConstructor
