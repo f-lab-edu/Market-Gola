e09b14f321f10b7320a061ccd209bb17513a5674
jieun-dev1 <jkdev1324@gmail.com>
2022-06-26 21:06:24 +0900

    chore : 불필요한 DB 관련 파일 삭제 (#3)
    MyBatisConfig 삭제
    -Config 파일 없이도 정상 구동 되어 삭제함.
    
    MyBatisUserRepository 삭제 후 userMapper 로 대체

--- src/main/java/com/marketGola/MarketgolaApplication.java
+++ src/main/java/com/marketGola/MarketgolaApplication.java
@@ -1,13 +1,11 @@
-package com.marketGola;
+package com.marketgola;
 
-import com.marketGola.user.config.MyBatisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import(MyBatisConfig.class)
 @SpringBootApplication
 public class MarketgolaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MarketgolaApplication.class, args);
     }
