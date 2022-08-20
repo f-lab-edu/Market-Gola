a8d047c085241ddb871eeaade873e795476c9702
jiwoo <sgo8308@gmail.com>
2022-08-20 12:45:02 +0900

    refactor(user): WebConfig.addArgumentResolvers()에서 불필요한 설정코드 제거

--- src/main/java/com/flab/marketgola/user/config/WebConfig.java
+++ src/main/java/com/flab/marketgola/user/config/WebConfig.java
@@ -23,7 +23,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(new LoginArgumentResolver());
     }
 }
