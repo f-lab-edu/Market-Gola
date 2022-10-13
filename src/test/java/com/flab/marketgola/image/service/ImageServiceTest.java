4661ef4b7c8acbdb073bbfc5b7aefd71d909d15c
jiwoo <sgo8308@gmail.com>
2022-10-13 23:37:55 +0900

    test(image): ImageServiceTest에서 AmazonS3 Mock 사용하도록 변경

--- src/test/java/com/flab/marketgola/image/service/ImageServiceTest.java
+++ src/test/java/com/flab/marketgola/image/service/ImageServiceTest.java
@@ -2,23 +2,26 @@ package com.flab.marketgola.image.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.flab.marketgola.TestRedisConfiguration;
+import com.amazonaws.services.s3.AmazonS3;
 import com.flab.marketgola.image.constant.TestImageFactory;
 import com.flab.marketgola.image.domain.MainImage;
 import java.io.FileNotFoundException;
 import java.util.regex.Pattern;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("unit")
-@SpringBootTest(classes = TestRedisConfiguration.class)
 class ImageServiceTest {
 
-    @Autowired
     ImageService imageService;
+
+    @BeforeEach
+    void setUp() {
+        imageService = new S3ImageService(Mockito.mock(AmazonS3.class));
+    }
 
     @DisplayName("이미지 저장 후 저장된 이미지의 이름을 반환한다.")
     @Test
