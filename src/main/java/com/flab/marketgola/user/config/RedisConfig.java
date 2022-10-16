3cd793f6285912ae0827e6c1116142e832a78ed4
jiwoo <sgo8308@gmail.com>
2022-10-16 18:45:50 +0900

    fix(user): redis에 저장한 세션이 제대로 역직렬화되지 않는 문제 해결
    Jackson2JsonRedisSerializer(LoginUser.class)를 redis에 저장된 hashValue를
    역직렬화하는 Serializer로 사용했으나, LogiunUser.class외에 lastAccessedTime과 같은
    다른 타입의 hashValue는 역직렬화에 실패하는 현상이 있어 GenericJackson2JsonRedisSerializer로 교체

--- src/main/java/com/flab/marketgola/user/config/RedisConfig.java
+++ src/main/java/com/flab/marketgola/user/config/RedisConfig.java
@@ -1,10 +1,9 @@
 package com.flab.marketgola.user.config;
 
-import com.flab.marketgola.user.domain.LoginUser;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
@@ -13,6 +12,6 @@ public class RedisConfig {
     @Bean
     @Qualifier("springSessionDefaultRedisSerializer")
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new Jackson2JsonRedisSerializer(LoginUser.class);
+        return new GenericJackson2JsonRedisSerializer();
     }
 }
