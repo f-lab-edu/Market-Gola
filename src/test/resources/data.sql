6c2d48229bf2601ba7caaaa7a5d459e275aef80e
jiwoo <sgo8308@gmail.com>
2022-10-21 12:03:17 +0900

    test(product): data.sql에 상품 카테고리 하나 더 추가
    상품 카테고리 관련 테스트를 용이하게 하기 위해서

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,6 +7,8 @@ insert into membership (level, point_rate)
 values ('black', 0.5);
 
 -- 상품 서비스용 초기 데이터
--- 과일 카테고리가 등록된다.
 insert into product_category (name)
 values ('과일');
+
+insert into product_category (name)
+values ('채소');
