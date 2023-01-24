a2333a432e6a68694b9f55dc9d64d999f9784692
jiwoo <sgo8308@gmail.com>
2023-01-24 19:12:32 +0900

    feat(user): user/domain 안에 클래스들에 JPA 어노테이션 적용

--- src/main/java/com/flab/marketgola/user/domain/Membership.java
+++ src/main/java/com/flab/marketgola/user/domain/Membership.java
@@ -1,13 +1,35 @@
 package com.flab.marketgola.user.domain;
 
 import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
-@Builder
 public class Membership {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, unique = true, nullable = false)
     private String level;
+
+    @Column(nullable = false)
     private BigDecimal pointRate;
+
+    @Builder
+    public Membership(Long id, String level, BigDecimal pointRate) {
+        this.id = id;
+        this.level = level;
+        this.pointRate = pointRate;
+    }
 }
