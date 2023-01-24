493877a57d01b383184ace47116709354fe7fab4
jiwoo <sgo8308@gmail.com>
2023-01-24 19:18:10 +0900

    feat(common): BaseEntity 생성

---
+++ src/main/java/com/flab/marketgola/common/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.flab.marketgola.common.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void beforePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
