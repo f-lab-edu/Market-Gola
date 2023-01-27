4eef4eb44fed984de44ccf156639d3c2e9ee25c2
jiwoo <sgo8308@gmail.com>
2023-01-27 21:25:56 +0900

    feat(product): ProductRepository에 미삭제 상품 찾는 findById 추가

--- src/main/java/com/flab/marketgola/product/repository/ProductRepository.java
+++ src/main/java/com/flab/marketgola/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.flab.marketgola.product.repository;
 
 import com.flab.marketgola.product.domain.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("update Product p set p.isDeleted=:isDeleted where p.displayProduct.id=:id")
     int updateStateByDisplayProductId(@Param("isDeleted") boolean isDeleted,
             @Param("id") Long displayProductId);
+
+    @Query("select p from Product p where p.id = :id and p.isDeleted = false")
+    Optional<Product> findById(@Param("id") Long id);
 }
