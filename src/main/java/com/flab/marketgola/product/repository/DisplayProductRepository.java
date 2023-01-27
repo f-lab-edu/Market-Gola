8ec7637eacfa7b6895884ad73c71ab9bff3db902
jiwoo <sgo8308@gmail.com>
2023-01-27 16:00:50 +0900

    feat(product): 전시 상품 DAO에 미삭제 상품 조회 findById 추가

--- src/main/java/com/flab/marketgola/product/repository/DisplayProductRepository.java
+++ src/main/java/com/flab/marketgola/product/repository/DisplayProductRepository.java
@@ -1,9 +1,14 @@
 package com.flab.marketgola.product.repository;
 
 import com.flab.marketgola.product.domain.DisplayProduct;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DisplayProductRepository extends JpaRepository<DisplayProduct, Long>,
         DisplayProductRepositoryCustom {
 
+    @Query("select dp from DisplayProduct dp join dp.products p where dp.id = :id and p.isDeleted = false")
+    Optional<DisplayProduct> findById(@Param("id") Long id);
 }
