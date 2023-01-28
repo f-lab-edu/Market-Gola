84435f632146a78e88b9d5d0a207b4dae04d4a2f
jiwoo <sgo8308@gmail.com>
2023-01-28 23:33:01 +0900

    chore(order): 아직 사용되지 않는 Membership 클래스의 생성자 제거
    테스트 되지 않은 생성자로 인해 jacoco 테스트 기준을 통과하지 못함

--- src/main/java/com/flab/marketgola/user/domain/Membership.java
+++ src/main/java/com/flab/marketgola/user/domain/Membership.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -26,11 +25,4 @@ public class Membership extends BaseEntity {
 
     @Column(nullable = false)
     private BigDecimal pointRate;
-
-    @Builder
-    public Membership(Long id, String level, BigDecimal pointRate) {
-        this.id = id;
-        this.level = level;
-        this.pointRate = pointRate;
-    }
 }
